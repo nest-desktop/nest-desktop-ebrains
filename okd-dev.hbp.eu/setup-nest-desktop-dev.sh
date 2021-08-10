f0bb11ebff3776fc8d62a402c267bb01abc9927e
Sebastian Spreizer <spreizer@web.de>
2021-08-10 15:56:04 +0200

    Rename to nest-simulator

--- okd-dev.hbp.eu/setup-nest-desktop-dev.sh
+++ okd-dev.hbp.eu/setup-nest-desktop-dev.sh
@@ -3,10 +3,10 @@
 #
 
 oc project nest-desktop
-oc delete is,dc,svc,route app nest-desktop nest-server apache2-oidc
+oc delete is,dc,svc,route app nest-simulator apache2-oidc
 oc delete configMap nest-desktop-apache2oidc
 
-oc new-app nestdesktop/app:3.0+nestdesktop/nest-server:3.0+nestdesktop/apache2-oidc:1.2
+oc new-app nestdesktop/app:3.0+nestdesktop/nest-simulator:3.0+nestdesktop/apache2-oidc:1.2
 oc new-app nest-desktop-apache2oidc-collab2-dev.yaml
 oc set env --from='configmap/nest-desktop-apache2oidc' dc/app
 oc create route edge --hostname='nest-desktop.apps-dev.hbp.eu' --port='8080-tcp' --service='app'
